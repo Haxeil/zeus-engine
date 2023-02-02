f3144e9b8fc98e3b52b74bb5c7ef41b77c267308
haxeil <lezharayman@gmail.com>
2023-02-02 17:25:36 +0100

    drawing triangle

--- zeus/src/main.rs
+++ zeus/src/main.rs
@@ -3,7 +3,12 @@ mod graphics;
 
 use crate::core::time;
 use glfw::{ffi::glfwGetWindowMonitor, Action, Context, Key, Window};
-use std::time::Instant;
+use std::{
+    ffi::{c_char, CStr, CString},
+    mem::size_of,
+    ptr::{null, null_mut},
+    time::Instant,
+};
 use time::Time;
 
 fn main() -> Result<(), String> {
@@ -15,19 +20,38 @@ fn main() -> Result<(), String> {
 
     let (mut window, events) = glfw.with_connected_monitors(|glfw, m| {
         let monitor = m.first().unwrap();
-        glfw.create_window(1280, 720, "title", glfw::WindowMode::FullScreen(monitor))
+        glfw.create_window(1280, 720, "title", glfw::WindowMode::Windowed)
             .expect("can't get window")
     });
-    
+
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
     // Make the window's context current
     window.make_current();
- 
+    unsafe {
+        let version = CStr::from_ptr(gl::GetString(gl::VERSION) as *const i8)
+            .to_str()
+            .unwrap();
+
+        println!("{:?}", version);
+
+        let mut buffer: u32 = 1;
+        let positions: [f32; 6] = [-0.5, -0.5, 0.0, 0.5, 0.5, -0.5];
+
+        gl::GenBuffers(1, &mut buffer);
+        gl::BindBuffer(gl::ARRAY_BUFFER, buffer);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            6 * size_of::<&f32>() as isize,
+            positions.as_ptr() as *const _,
+            gl::STATIC_DRAW,
+        )
+    }
     window.set_key_polling(true);
     // insuring that the the window won't stuck at the machine refrech rate;
     glfw.set_swap_interval(glfw::SwapInterval::None);
     let mut i = 0_f32;
     // Loop until the user closes the window
+
     while !window.should_close() {
         time.update();
         time.frames += 1;
@@ -36,13 +60,46 @@ fn main() -> Result<(), String> {
             time.updates += 1;
             time.delta -= 1.0;
             i = (i + 0.01) % 1.0;
+            unsafe {
+                // gl::ClearColor(i, 0.3, 0.3 / i, 1.0);
+                // gl::Clear(gl::COLOR_BUFFER_BIT);
+                gl::DrawArrays(gl::TRIANGLES, 0, 3);
+                gl::EnableVertexAttribArray(0);
+
+                gl::VertexAttribPointer(0, 2, gl::FLOAT, gl::FALSE, 8, 0 as *const _);
+
+                let vertex_shader = String::from(
+                    "
+                        #version 330 core\n
+                        \n
+                        layout(location = 0) in vec4 position;
+                        \n
+                        void main()\n
+                        {\n
+                            gl_Position = position;\n
+                        }\n
+                        ",
+                );
+                let fragment_shader = String::from(
+                    "
+                        #version 330 core\n
+                        \n
+                        layout(location = 0) out vec4 color;
+                        \n
+                        void main()\n
+                        {\n
+                            color = vec4(1.0, 0.0, 0.0, 1.0);\n
+                        }\n
+                        ",
+                );
+
+                let shader: u32 = create_shader(vertex_shader, fragment_shader);
+                gl::UseProgram(shader);
+            }
         }
 
         time.frames += 1;
-        unsafe {
-            gl::ClearColor(i, 0.3, 0.3 / i, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        }
+
         // Poll for and process events
         glfw.poll_events();
         window.swap_buffers();
@@ -73,24 +130,51 @@ fn main() -> Result<(), String> {
     Ok(())
 }
 
-// let window = WindowBuilder::new().build(&event_loop).unwrap();
+unsafe fn create_shader(vertex_shader: String, fragment_shader: String) -> u32 {
+    let program = gl::CreateProgram();
+    let vs = compile_shader(gl::VERTEX_SHADER, vertex_shader);
+    let fs = compile_shader(gl::FRAGMENT_SHADER, fragment_shader);
+    // Attach Shaders;
+    gl::AttachShader(program, vs);
+    gl::AttachShader(program, fs);
+    gl::LinkProgram(program);
+    gl::ValidateProgram(program);
+    // Drop Shaders;
+    gl::DeleteShader(vs);
+    gl::DeleteShader(vs);
 
-//     event_loop.run(move |event, _, control_flow| match event {
-//         Event::WindowEvent {
-//             ref event,
-//             window_id,
-//         } if window_id == window.id() => match event {
-//             WindowEvent::CloseRequested
-//             | WindowEvent::KeyboardInput {
-//                 input:
-//                     KeyboardInput {
-//                         state: ElementState::Pressed,
-//                         virtual_keycode: Some(VirtualKeyCode::Escape),
-//                         ..
-//                     },
-//                 ..
-//             } => *control_flow = ControlFlow::Exit,
-//             _ => {}
-//         },
-//         _ => {}
-//     });
+    program
+}
+
+unsafe fn compile_shader(c_type: u32, source: String) -> u32 {
+    let id = gl::CreateShader(c_type);
+    // let src = source.as_bytes().as_ptr() as *const *const i8;
+    let c_str = CString::new(source).unwrap();
+    let src = c_str.as_ptr();
+    let s_ptr = &src as *const *const i8;
+
+    gl::ShaderSource(id, 1, s_ptr, std::ptr::null());
+    gl::CompileShader(id);
+
+    let mut result = 0;
+    gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut result as *mut i32);
+
+    if result as u8 == gl::FALSE {
+        let mut length: i32 = 0;
+        gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut length as *mut _);
+        let message: *mut c_char = CString::new("").unwrap().into_raw();
+        gl::GetShaderInfoLog(id, length, &mut length as *mut _, message);
+
+        println!(
+            "Failed to compile: {}",
+            CStr::from_ptr(message).to_str().unwrap().to_owned()
+        );
+
+        gl::DeleteShader(id);
+        return 0;
+    }
+    id
+}
+
+
+// TODO: Abstract all of this into Screen struct
