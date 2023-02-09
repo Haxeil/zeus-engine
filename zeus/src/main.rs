f4829690f1ea5a17c646d5dd05c3e0f8196fca34
haxeil <lezharayman@gmail.com>
2023-02-09 01:02:59 +0100

    error handling for opengl errors

--- zeus/src/main.rs
+++ zeus/src/main.rs
@@ -5,20 +5,32 @@ mod graphics;
 use crate::core::time;
 use glfw::{Action, Context, Key};
 use std::{
-    io,
+    error::Error,
     ffi::{c_char, CStr, CString},
     mem::size_of,
     ptr::{null},
     time::Instant, alloc::{alloc, Layout},
 };
 use std::fs::File;
-use std::io::{BufRead, BufReader, Error};
+use std::io::{BufRead, BufReader};
 use std::ops::Add;
 use std::path::Path;
 use time::Time;
 use crate::core::shader_source::ShaderSource;
 
-fn main() -> Result<(), io::Error> {
+
+macro_rules! log_gl_error {
+
+    ($func:expr) => {
+            gl_clear_errors();
+            $func;
+            gl_check_error(stringify!($func), file!(), line!());
+
+
+    };
+}
+
+fn main() -> Result<(), Box::<dyn Error>> {
     env_logger::init();
     let mut time = Time::default();
     let mut timer = Instant::now();
@@ -53,11 +65,11 @@ fn main() -> Result<(), io::Error> {
     let mut vbo = 0;
     let mut ebo = 0;
     unsafe {
-        gl::GenVertexArrays(1, &mut vao);
-        gl::GenBuffers(1, &mut vbo);
-        gl::GenBuffers(1, &mut ebo);
+        log_gl_error!(gl::GenVertexArrays(1, &mut vao));
+        log_gl_error!(gl::GenBuffers(1, &mut vbo));
+        log_gl_error!(gl::GenBuffers(1, &mut ebo));
         // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-        gl::BindVertexArray(vao);
+        log_gl_error!(gl::BindVertexArray(vao));
     }
 
     window.make_current();
@@ -81,36 +93,36 @@ fn main() -> Result<(), io::Error> {
         ];
 
         let mut buffer: u32 = 0;
-        gl::GenBuffers(1, &mut buffer);
-        gl::BindBuffer(gl::ARRAY_BUFFER, buffer);
-        gl::BufferData(
+        log_gl_error!(gl::GenBuffers(1, &mut buffer));
+        log_gl_error!(gl::BindBuffer(gl::ARRAY_BUFFER, buffer));
+        log_gl_error!(gl::BufferData(
             gl::ARRAY_BUFFER,
             12 as isize * size_of::<f32>() as isize,
             positions.as_ptr() as *const _,
             gl::STATIC_DRAW,
-        );
+        ));
 
-        gl::VertexAttribPointer(0, 2, gl::FLOAT, gl::FALSE, 8, 0 as *const _);
+        log_gl_error!(gl::VertexAttribPointer(0, 2, gl::FLOAT, gl::FALSE, 8, 0 as *const _));
 
         let shaders = parse_shader("src/res/shaders/Basic.shader")?;
-        let shader: u32 = create_shader(shaders.vertex_shader, shaders.fragment_shader);
-        gl::UseProgram(shader);
+        let shader: u32 = create_shader(shaders.vertex_shader, shaders.fragment_shader)?;
+        log_gl_error!(gl::UseProgram(shader));
 
         let mut ibo: u32 = 0;
-        gl::GenBuffers(1, &mut ibo);
-        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ibo);
-        gl::BufferData(
+        log_gl_error!(gl::GenBuffers(1, &mut ibo));
+        log_gl_error!(gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ibo));
+        log_gl_error!(gl::BufferData(
             gl::ELEMENT_ARRAY_BUFFER,
             6 as isize * size_of::<f32>() as isize,
             indicies.as_ptr() as *const _,
             gl::STATIC_DRAW,
-        );
+        ));
 
     }
     window.set_key_polling(true);
     // insuring that the the window won't stuck at the machine refresh rate;
 
-    glfw.set_swap_interval(glfw::SwapInterval::None);
+    glfw.set_swap_interval(glfw::SwapInterval::Adaptive);
     let mut i = 0_f32;
 
     while !window.should_close() {
@@ -122,10 +134,10 @@ fn main() -> Result<(), io::Error> {
             time.delta -= 1.0;
             i = (i + 0.01) % 1.0;
             unsafe {
-                gl::ClearColor(i, 0.3, 0.3 / i, 1.0);
-                gl::Clear(gl::COLOR_BUFFER_BIT);
-                gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, null());
-                gl::EnableVertexAttribArray(0);
+                log_gl_error!(gl::ClearColor(i, 0.3, 0.3 / i, 1.0));
+                log_gl_error!(gl::Clear(gl::COLOR_BUFFER_BIT));
+                log_gl_error!(gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, null()));
+                log_gl_error!(gl::EnableVertexAttribArray(0));
             }
         }
 
@@ -139,7 +151,7 @@ fn main() -> Result<(), io::Error> {
             timer = Instant::now();
 
             window.set_title(&format!(
-                "Frimi d zab | {} up, {} fps, {} delta",
+                "Engine | {} up, {} fps, {} delta",
                 time.updates, time.frames, time.delta
             ));
 
@@ -161,53 +173,53 @@ fn main() -> Result<(), io::Error> {
     Ok(())
 }
 
-unsafe fn create_shader(vertex_shader: String, fragment_shader: String) -> u32 {
+unsafe fn create_shader(vertex_shader: String, fragment_shader: String) -> Result<u32, Box<dyn Error>> {
     let program = gl::CreateProgram();
-    let vs = compile_shader(gl::VERTEX_SHADER, vertex_shader);
-    let fs = compile_shader(gl::FRAGMENT_SHADER, fragment_shader);
+    let vs = compile_shader(gl::VERTEX_SHADER, vertex_shader)?;
+    let fs = compile_shader(gl::FRAGMENT_SHADER, fragment_shader)?;
     // Attach Shaders;
-    gl::AttachShader(program, vs);
-    gl::AttachShader(program, fs);
-    gl::LinkProgram(program);
-    gl::ValidateProgram(program);
+    log_gl_error!(gl::AttachShader(program, vs));
+    log_gl_error!(gl::AttachShader(program, fs));
+    log_gl_error!(gl::LinkProgram(program));
+    log_gl_error!(gl::ValidateProgram(program));
     // Drop Shaders;
-    gl::DeleteShader(vs);
-    gl::DeleteShader(vs);
+    log_gl_error!(gl::DeleteShader(vs));
+    log_gl_error!(gl::DeleteShader(vs));
 
-    program
+    Ok(program)
 }
 
-unsafe fn compile_shader(c_type: u32, source: String) -> u32 {
+unsafe fn compile_shader(c_type: u32, source: String) -> Result<u32, Box<dyn Error>> {
     let id = gl::CreateShader(c_type);
     // let src = source.as_bytes().as_ptr() as *const *const i8;
-    let c_string = alloc::ffi::CString::new(source).unwrap();
+    let c_string = alloc::ffi::CString::new(source)?;
     let source = c_string.as_ptr();
     let source = &source as *const *const i8;
 
-    gl::ShaderSource(id, 1, source, null());
-    gl::CompileShader(id);
+    log_gl_error!(gl::ShaderSource(id, 1, source, null()));
+    log_gl_error!(gl::CompileShader(id));
 
     let mut result = 0;
-    gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut result as *mut i32);
+    log_gl_error!(gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut result as *mut i32));
 
     if result as u8 == gl::FALSE {
         let mut length: i32 = 0;
-        gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut length as *mut _);
-        let layout = Layout::from_size_align(length.try_into().unwrap(), 1).unwrap();
+        log_gl_error!(gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut length as *mut _));
+        let layout = Layout::from_size_align(length.try_into()?, 1)?;
         let message: *mut c_char = alloc(layout) as *mut i8;
-        gl::GetShaderInfoLog(id, length, &mut length as *mut _, message);
+        log_gl_error!(gl::GetShaderInfoLog(id, length, &mut length as *mut _, message));
         println!(
             "Failed to compile: {}",
-            CStr::from_ptr(message).to_str().unwrap()
+            CStr::from_ptr(message).to_str()?
         );
 
-        gl::DeleteProgram(id);
-        return 0;
+        log_gl_error!(gl::DeleteProgram(id));
+        return Ok(0);
     }
-    id
+    Ok(id)
 }
 
-unsafe fn parse_shader(path: &str) -> Result<ShaderSource, Error> {
+unsafe fn parse_shader(path: &str) -> Result<ShaderSource, Box<dyn Error>> {
     let file = File::open(path)?;
     let mut shaders = ShaderSource::new();
     let reader = BufReader::new(file);
@@ -238,5 +250,22 @@ unsafe fn parse_shader(path: &str) -> Result<ShaderSource, Error> {
 
     Ok(shaders)
 }
+
+unsafe fn gl_clear_errors() {
+    while gl::GetError() != gl::NO_ERROR {}
+}
+
+unsafe fn gl_check_error(func_name: &str, file_name: &str, line: u32) {
+    loop {
+        let error = gl::GetError();
+        if error == 0 {
+            break;
+        }
+        println!("open_gl Error: {}, func: {}, file_name: {}, line: {}", error, func_name, file_name, line);
+        debug_assert!(false);
+    }
+
+}
+
 
 // TODO: Abstract all of this into Screen struct
