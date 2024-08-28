b2ed4297b6e358ed1bfa68d7c32434d4c83e27c9
Ayman Lezhar <58956198+Haxeil@users.noreply.github.com>
2024-08-28 23:40:00 +0200

    buffer struct

--- src/graphics/buffers/buffer.rs
+++ src/graphics/buffers/buffer.rs
@@ -5,21 +5,29 @@ use gl::types::*;
 pub struct Buffer {
 
     buffer_id: GLuint,
-    component_count: GLuint,
+    pub component_count: GLuint,
 
 }
 
 impl Buffer {
-    pub fn vertex_buffer(&mut self, data: *const GLfloat, count: GLsizeiptr, component_count: GLuint) {
-        self.component_count = component_count;
+    pub fn from(data: *const GLfloat, count: GLsizeiptr, component_count: GLuint) -> Self {
+
+        let component_count = component_count;
+        let mut buffer_id: GLuint = 0;
 
         unsafe {
-            gl::GenBuffers(1, &mut self.buffer_id);
-            gl::BindBuffer(gl::ARRAY_BUFFER, self.buffer_id);
+            gl::GenBuffers(1, &mut buffer_id);
+            gl::BindBuffer(gl::ARRAY_BUFFER, buffer_id);
             let size = count * size_of::<GLfloat>() as GLsizeiptr ;
             gl::BufferData(gl::ARRAY_BUFFER, size, data as *const _, gl::STATIC_DRAW);
             // Unbindbuffer;
             gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        }
+
+        Self {
+            buffer_id,
+            component_count,
+
         }
     }
 
