426473394c140385db87d839e412df49a4b18578
Ayman Lezhar <58956198+Haxeil@users.noreply.github.com>
2024-09-01 10:23:39 +0200

    abstracted using buffer in Renderable2D

--- src/graphics/renderable2d.rs
+++ src/graphics/renderable2d.rs
@@ -15,14 +15,36 @@ pub struct Renderable2D<'a> {
 impl<'a> Renderable2D<'a> {
 
     pub fn from(position: Vec3, size: Vec2, color: Vec4) -> Self {
+        let mut vertex_array = VertexArray::new();
 
         let indicies: [u16; 6] = [0, 1, 2, 2, 3, 0];
+
+        let verticies = [
+            0.0, 0.0, 0.0,
+            0.0, position.y, 0.0,
+            position.x, position.y, 0.0,
+            position.x, 0.0, 0.0,
+
+        ];
+
+        let colors = [
+            color.x, color.y, color.z, color.w,
+            color.x, color.y, color.z, color.w,
+            color.x, color.y, color.z, color.w,
+            color.x, color.y, color.z, color.w,
+
+        ];
+
+        vertex_array.add_buffer(&mut Buffer::from(&verticies, 12, 3), 0);
+        vertex_array.add_buffer(&mut Buffer::from(&colors, 16, 4), 1);
+
+
         Self {
             position,
             size,
             color,
 
-            vertex_array: VertexArray::new(),
+            vertex_array,
             index_buffer: IndexBuffer::from(&indicies, 6),
             
         }
