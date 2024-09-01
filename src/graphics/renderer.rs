695ed56d81b396fa9608e025eb7e3d53596c540d
Ayman Lezhar <58956198+Haxeil@users.noreply.github.com>
2024-09-02 01:40:48 +0200

    Renderer 2D

--- src/graphics/renderer.rs
+++ src/graphics/renderer.rs
@@ -1,3 +1,25 @@
-struct Renderer {
-    
+use std::{cell::{Cell, RefCell}, collections::VecDeque};
+
+use super::renderable2d::Renderable2D;
+
+pub struct Renderer<'a> {
+    pub render_queue: VecDeque<Renderable2D<'a>>,
+
+}   
+
+impl Renderer<'_> {
+
+    pub fn new() -> Self {
+        Self {
+            render_queue: VecDeque::new()
+        }
+    }
 }
+
+
+// pub trait Render<'a> {
+//     fn submit(&mut self, renderable2d: &'a Renderable2D<'a>);
+//     fn flush(&mut self);
+
+// }
+
