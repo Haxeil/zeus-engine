eaf4ee7f244d63f5fdbefaa70701b6e6967695b4
Ayman Lezhar <58956198+Haxeil@users.noreply.github.com>
2024-09-03 13:28:09 +0200

    renderable

--- src/graphics/renderable2d.rs
+++ src/graphics/renderable2d.rs
@@ -10,55 +10,27 @@ pub struct VertexData {
     color: Vec4,
 }
 
-pub struct Renderable2D<'a> {
+pub struct Renderable2D {
 
     pub size: Vec2,
     pub position: Vec3,
     pub color: Vec4,
 
-    pub shader: &'a Shader,
 
-    pub vertex_array: VertexArray<'a>,
-    pub index_buffer: IndexBuffer,
 }
 
 
 
-impl<'a> Renderable2D<'a> {
+impl Renderable2D {
 
-    pub fn from(position: Vec3, size: Vec2, color: Vec4, shader: &'a Shader) -> Self {
-        let mut vertex_array = VertexArray::new();
-
-        let indicies: [u16; 6] = [0, 1, 2, 2, 3, 0];
-
-        let verticies = [
-            0.0, 0.0, 0.0,
-            0.0, size.y, 0.0,
-            size.x, size.y, 0.0,
-            size.x, 0.0, 0.0,
-
-        ];
-
-        let colors = [
-            color.x, color.y, color.z, color.w,
-            color.x, color.y, color.z, color.w,
-            color.x, color.y, color.z, color.w,
-            color.x, color.y, color.z, color.w,
-
-        ];
-
-        vertex_array.add_buffer(&mut Buffer::from(&verticies, 12, 3), 0);
-        vertex_array.add_buffer(&mut Buffer::from(&colors, 16, 4), 1);
-
+    pub fn from(position: Vec3, size: Vec2, color: Vec4) -> Self {
 
         Self {
             position,
             size,
             color,
 
-            vertex_array,
-            index_buffer: IndexBuffer::from(&indicies, 6),
-            shader,
+
             
         }
     }
