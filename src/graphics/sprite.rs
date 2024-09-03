5dd1a9edb3a5caf96fe1aa70df64277dd7dcac36
Ayman Lezhar <58956198+Haxeil@users.noreply.github.com>
2024-09-03 13:24:30 +0200

    sprite

---
+++ src/graphics/sprite.rs
@@ -0,0 +1,15 @@
+use super::{renderable2d::Renderable2D, shader::Shader};
+
+
+
+pub struct Sprite {
+    renderable2d: Renderable2D,
+
+}
+
+
+impl<'a> Sprite {
+    pub fn from(shader: &'a Shader, renderable: Renderable2D) {
+
+    }
+}
