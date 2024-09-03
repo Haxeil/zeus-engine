760b57d31082ffc688262f2ac9bc17add4d73cd7
Ayman Lezhar <58956198+Haxeil@users.noreply.github.com>
2024-09-03 13:26:51 +0200

    updated simple_renderer to support Static sprite

--- src/graphics/simple2d_renderer.rs
+++ src/graphics/simple2d_renderer.rs
@@ -2,13 +2,13 @@ use std::{cell::{Cell, RefCell, RefMut}, collections::VecDeque, os::raw::c_void,
 
 use crate::{mat4::Mat4, vec3::Vec3, vec4::Vec4};
 
-use super::{renderable2d::Renderable2D, renderer::{Render, Renderer}};
+use super::{renderable2d::Renderable2D, renderer::{Render, Renderer}, sprite, static_sprite::StaticSprite};
 use gl::types::*;
 pub struct Simple2dRenderer<'a> {
     pub renderer: Renderer<'a>,
 }
 
-impl Simple2dRenderer<'_> {
+impl<'a> Simple2dRenderer<'a> {
     pub fn new() -> Self {
         Self {
            renderer: Renderer::new(),
@@ -21,35 +21,28 @@ impl Simple2dRenderer<'_> {
 impl<'a> Render<'a> for Simple2dRenderer<'a> {
 
 
-    fn submit(&mut self, renderable2d: Rc<RefCell<Renderable2D<'a>>>) {
-        self.renderer.render_queue.push_back(renderable2d);
+    fn submit(&mut self, sprite: &'a StaticSprite) {
+        self.renderer.render_queue.push_back(sprite);
     }
 
     fn flush(&mut self) {
         while !self.renderer.render_queue.is_empty() {
-            if let Some(renderable) = self.renderer.render_queue.front_mut() {
+            if let Some(sprite) = self.renderer.render_queue.front_mut() {
 
-                let renderable = renderable.borrow_mut();
 
-                renderable.vertex_array.bind();
+                sprite.vertex_array.bind();
                 
-                renderable.index_buffer.bind();
+                sprite.index_buffer.bind();
 
-                renderable.shader.set_uniform_mat4("ml_matrix", Mat4::translation(&renderable.position));
+                sprite.shader.set_uniform_mat4("ml_matrix", Mat4::translation(&sprite.renderable2d.position));
                 unsafe {
-                    gl::DrawElements(gl::TRIANGLES, renderable.index_buffer.count as i32, gl::UNSIGNED_SHORT, 0  as *const c_void);
+                    gl::DrawElements(gl::TRIANGLES, sprite.index_buffer.count as i32, gl::UNSIGNED_SHORT, 0  as *const c_void);
 
                 }
-
-                renderable.vertex_array.unbind();
-                renderable.index_buffer.unbind();
-
-
+                sprite.vertex_array.unbind();
+                sprite.index_buffer.unbind();
             }
-
             self.renderer.render_queue.pop_front();
-
-            
         }
     }
 }
