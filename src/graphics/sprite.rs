5908356d738cff2b8c1ca00aa4a6ce5d2f6552d0
Ayman Lezhar <58956198+Haxeil@users.noreply.github.com>
2024-09-05 07:51:57 +0200

    simple Sprite struct

--- src/graphics/sprite.rs
+++ src/graphics/sprite.rs
@@ -1,15 +1,25 @@
-use super::{renderable2d::Renderable2D, shader::Shader};
+use super::{renderable2d::{Renderable, Renderable2D}, shader::Shader};
 
 
 
 pub struct Sprite {
-    renderable2d: Renderable2D,
+    pub renderable2d: Renderable2D,
 
 }
 
 
-impl<'a> Sprite {
-    pub fn from(shader: &'a Shader, renderable: Renderable2D) {
-
+impl Sprite {
+    pub fn from(renderable: Renderable2D) -> Self {
+        Self {
+            renderable2d: renderable
+        }
     }
 }
+
+impl Renderable for Sprite {
+    fn get_renderable(&self) -> &Renderable2D {
+        
+        &self.renderable2d
+    }
+}
+
